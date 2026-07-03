c1616c7111f117dd70bf5a0b7fa8a1345a3e7701
Dinh <nquydinh@yahoo.com>
2026-07-03 12:33:14 +0930

    planning(add comments for 2 functions)

---
+++ ReportUserLogins/reportUsersLogins.py
@@ -0,0 +1,6 @@
+# This program will report all currently logged in to the machine
+import os
+
+# Function to the machines that are currently logged in by the users
+
+# Function to generate the report of the machines and users currently logged in
